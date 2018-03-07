b43618bb7a023799cfd93456431d137685203113
gdcp <gdcp@m18.local>
2018-03-07 15:49:15 +0800

    1.duolhfkjdsf

--- 123/ViewController.swift
+++ 123/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("ABC")
+        print("DEF")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +21,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
 
 }
